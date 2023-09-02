178944d09d0b70a265ab598471fefa555949fb05
betha <bethaniebell8474@gmail.com>
2023-09-02 13:47:59 +0800

    s

--- Dispatcher/Middlewares/api/RequestChooserMiddleware.cs
+++ Dispatcher/Middlewares/api/RequestChooserMiddleware.cs
@@ -20,8 +20,6 @@ public class RequestChooserMiddleware
     public async Task InvokeAsync(HttpContext context)
     {
         var random = new Random();
-
-
         if (_repository.Count < 1)
         {
             var delayParts = _limit.WaitSeconds * 1000 / 10;
@@ -31,13 +29,18 @@ public class RequestChooserMiddleware
                 part++;
                 await Task.Delay(delayParts);
             }
-            var completion = Completion.GetDefaultOrExample($"当前动态池中没有实际请求密钥，请重试。（程序错误）"+
-                    $"\n这往往是因为IIS的应用程序池正在重新启动导致的，IIS会在长时间没有请求时，回收应用程序池。" +
-                    $"\n 不过，随着当前请求，应用程序池将会重新启动，请耐心等待或者重新请求。" +
-                    $"\n 本网站配置了Hangfire定时任务来进行密钥加载，所以可能会加载若干秒。" +
-                    $"\n 请坐和放宽。");
-            await GptReply.Reply(context,completion);
-            return;
+
+            if (_repository.Count < 1)
+            {
+                var completion = Completion.GetDefaultOrExample($"已经等待{_limit.WaitSeconds}秒\n" +
+                                                                $"当前动态池中没有实际请求密钥，请重试。（程序错误）"+
+                                                                $"\n这往往是因为IIS的应用程序池正在重新启动导致的，IIS会在长时间没有请求时，回收应用程序池。" +
+                                                                $"\n 不过，随着当前请求，应用程序池将会重新启动，请耐心等待或者重新请求。" +
+                                                                $"\n 本网站配置了Hangfire定时任务来进行密钥加载，所以可能会加载若干秒。" +
+                                                                $"\n 请坐和放宽。");
+                await GptReply.Reply(context,completion);
+                return;
+            }
         }
         var index = random.Next(_repository.Count);
         var pickedKey = _repository.OpenPoolKeys[index];
